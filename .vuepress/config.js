d8acf2e6ab1f6d655e29804a3fc0edbbdf81b43b
jkatagi <kemisknkn37@gmail.com>
2018-10-10 16:53:35 +0900

    Revert "upload GEE tutorial"
    This reverts commit 47e92876390927d7244fe9eed0ab9aa80d9c543f.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,12 +4,7 @@ module.exports = {
         nav: [
             { text: 'Home', link: '/' },
             { text: 'ゼロから始める衛星画像解析', link: '/contents/google_earth_engine/' },
-            { text: 'GitHub', link: 'https://github.com/jkatagi' }
+            { text: 'Github', link: 'https://github.com/jkatagi' }
         ]
-    },
-    markdown: {
-        config: md => {
-            md.use(require("markdown-it-katex"));
-        }
     }
 }
