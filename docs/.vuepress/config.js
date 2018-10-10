36c53df91b5dba01b32278a46122b8280d9fab07
jkatagi <kemisknkn37@gmail.com>
2018-10-10 13:19:49 +0900

    finish setting

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: "jkatagi's pages",
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'ゼロから始める衛星画像解析', link: '/contents/google_earth_engine/' },
+            { text: 'Github', link: 'https://github.com/jkatagi' }
+        ]
+    }
+}
